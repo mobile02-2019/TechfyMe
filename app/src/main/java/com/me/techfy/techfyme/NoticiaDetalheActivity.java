2f482090951e8d094ea28ea9ccd8b434565e1471
Joao Ricardi <JoaoRicardi@MacBook-Air-de-Joao.local>
2018-10-29 14:06:18 -0300

    botao fab com o toast funcionando

--- app/src/main/java/com/me/techfy/techfyme/NoticiaDetalheActivity.java
+++ app/src/main/java/com/me/techfy/techfyme/NoticiaDetalheActivity.java
@@ -1,14 +1,20 @@
 package com.me.techfy.techfyme;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.me.techfy.techfyme.modelo.Noticia;
 
 import java.util.Date;
 
 public class NoticiaDetalheActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +27,24 @@ public class NoticiaDetalheActivity extends AppCompatActivity {
         TextView fonte = findViewById(R.id.text_fonte_noticiadetail_id);
         TextView dataNoticia = findViewById(R.id.text_data_noticiadetail_id);
         TextView textoCompleto = findViewById(R.id.text_textocompleto_noticiadetail_id);
+        FloatingActionButton fab = findViewById(R.id.fab_detalhe_noticia_id);
 
         titulo.setText(bundle.getString(NoticiaFragment.NOTICIA_TITULO));
         fonte.setText(bundle.getString(NoticiaFragment.NOTICIA_FONTE));
         dataNoticia.setText(bundle.getString(NoticiaFragment.NOTICIA_DATA));
         textoCompleto.setText(bundle.getString(NoticiaFragment.NOTICIA_TEXTO));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Noticia adicionado aos favoritos", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
 
 
     }
