9de3fce29db75cd87483734caccbe48c03cfa893
LuaraRamos <luara.m.ramos@gmail.com>
2018-11-22 15:16:50 -0200

    subindologin

--- app/src/main/java/com/me/techfy/techfyme/LoginActivity.java
+++ app/src/main/java/com/me/techfy/techfyme/LoginActivity.java
@@ -25,11 +25,18 @@ import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.me.techfy.techfyme.CadastroActivity;
 import com.me.techfy.techfyme.HomeActivity;
 import com.me.techfy.techfyme.R;
@@ -37,11 +44,14 @@ import com.me.techfy.techfyme.R;
 public class LoginActivity extends AppCompatActivity {
 
     public static final String CHAVE_EMAIL = "chave_email";
+    private static final int RC_SIGN_IN = 1000 ;
     Button botaoLogin;
     TextView cadastrar;
     private static final String TAG = "Login";
     private FirebaseAuth mAuth;
     private CallbackManager callbackManager;
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,13 +106,27 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        Button btnLoginGoogle = findViewById(R.id.btn_login_google);
+
+        btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
+
+        //        inicio login com google: - inicio do codigo
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
 
     @Override
     public void onStart() {
@@ -120,6 +144,63 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    // METODOS LOGIN GOOGLE
+
+    // updateUI(user); foi subtituido no google para goToHome
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToHome();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+//                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    // FIM METODOS LOGIN GOOGLE
     private void goToHome(){
         Intent intent = new Intent(this,MenuHomeActivity.class);
         startActivity(intent);
@@ -130,6 +211,8 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
 
 
     public void loginClicado (){
